e51ea53204cdce7e513541f4c746a1ad302ef8d2
khrystyna.ozarko <khrystyna.ozarko@intellias.com>
2023-03-14 22:36:32 +0200

    add upcoming task endpoint

--- src/InteractionOfficeBot.Core/MsGraph/TodoTaskRepository.cs
+++ src/InteractionOfficeBot.Core/MsGraph/TodoTaskRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Graph;
+﻿using InteractionOfficeBot.Core.Model;
+using Microsoft.Graph;
 
 namespace InteractionOfficeBot.Core.MsGraph;
 
@@ -11,17 +12,42 @@ public class TodoTaskRepository
         _graphServiceClient = graphServiceClient;
     }
 
-    public async Task<List<TodoTask>> GetTodoTasks()
+    public async Task<List<TodoTaskEntity>> GetTodoTasks()
     {
         var list = await _graphServiceClient.Me.Todo.Lists.Request().GetAsync();
         var listId = list.FirstOrDefault().Id;
 
         var result = await _graphServiceClient.Me.Todo.Lists[listId].Tasks.Request().GetAsync();
-        
-        return result.ToList();
+
+        var todoTask = result.Select(x => new TodoTaskEntity
+        {
+            Title = x.Title,
+            ReminderDateTime = x.ReminderDateTime,
+            Status = x.Status,
+        }).ToList();
+
+        return todoTask;
     }
 
-    //todo reminder
+    public async Task<List<TodoTaskEntity>> GetUpcomingTodoTasks(DateTime? reminderTime)
+    {
+        var list = await _graphServiceClient.Me.Todo.Lists.Request().GetAsync();
+        var listId = list.FirstOrDefault().Id;
+
+        var result = await _graphServiceClient.Me.Todo.Lists[listId].Tasks.Request().GetAsync();
+
+        var upcomingTasks = result
+            .Where(x => x.ReminderDateTime == DateTimeTimeZone.FromDateTime(reminderTime ?? DateTime.Today))
+            .Select(x => new TodoTaskEntity
+            {
+                Title = x.Title,
+                ReminderDateTime = x.ReminderDateTime,
+                Status = x.Status,
+            }).ToList();
+
+        return upcomingTasks;
+    }
+
     public async Task CreateTodoTask(string title, DateTime? reminderTime)
     {
         var list = await _graphServiceClient.Me.Todo.Lists.Request().GetAsync();
