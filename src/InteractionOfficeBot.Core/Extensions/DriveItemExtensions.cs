02f5d2a5471da7d7a2cf6d3a2aaef4c01b0433a4
Alexander <alx875@gmail.com>
2023-03-16 01:06:09 +0200

    add exception handling

--- src/InteractionOfficeBot.Core/Extensions/DriveItemExtensions.cs
+++ src/InteractionOfficeBot.Core/Extensions/DriveItemExtensions.cs
@@ -6,15 +6,15 @@ namespace InteractionOfficeBot.Core.Extensions
     {
         public static string GetFullPath(this DriveItem driveItem)
         {
-            var folderPath = driveItem.ParentReference?.Path.Substring(driveItem.ParentReference.Path.IndexOf(':') + 1);
+            var folderPath = driveItem.ParentReference?.Path.Substring(driveItem.ParentReference.Path.IndexOf(':') + 1) ?? "/";
             var fileName = driveItem.GetDisplayName();
 
-            return string.Join("\\", new[] { folderPath, fileName }.Where(x => !string.IsNullOrEmpty(x)));
+            return string.Join("/", new[] { folderPath, fileName }.Where(x => !string.IsNullOrEmpty(x)));
         }
 
         public static string GetDisplayName(this DriveItem driveItem)
         {
-            return driveItem.Folder != null ? driveItem.Name : $"{driveItem.Name}\\";
+            return driveItem.Folder == null ? driveItem.Name : $"{driveItem.Name}/";
         }
 
         public static bool IsFileOrFolder(this DriveItem driveItem)
